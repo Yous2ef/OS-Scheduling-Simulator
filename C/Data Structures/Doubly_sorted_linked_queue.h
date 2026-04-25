106aa62b26ad67d5906727b72b695a6ab569e31c
0xfreed0m <ammarharris121t@gmail.com>
2026-04-25 19:02:21 +0300

    Doubly sorted linked queue in case converting the algorithm to linked list based

--- C/Data Structures/Doubly_sorted_linked_queue.h
+++ C/Data Structures/Doubly_sorted_linked_queue.h
@@ -1,8 +1,24 @@
-//
-// Created by freedom on 4/16/26.
-//
-
 #ifndef OS_SCHEDULING_SIMULATOR_DOUBLY_SORTED_LINKED_QUEUE_H
 #define OS_SCHEDULING_SIMULATOR_DOUBLY_SORTED_LINKED_QUEUE_H
+
+typedef char Data;
+
+typedef struct nodeS {
+    int key;
+    Data data;
+    struct nodeS *next;
+    struct nodeS *prev;
+}DoubleNode;
+
+typedef struct{
+    DoubleNode *head;
+}DoublyList;
+
+void createList(DoublyList* l);
+int isDSListEmpty(DoublyList l);
+int isDSListFull(DoublyList l);
+void insertDSList(DoublyList* l, int key, Data data);
+Data retrieveDSList(DoublyList* l, int key);
+void printDSList(DoublyList l);
 
 #endif //OS_SCHEDULING_SIMULATOR_DOUBLY_SORTED_LINKED_QUEUE_H
